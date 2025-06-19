88bcd35db966450326faae37698cc63168067865
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-06-19 16:32:35 -0300

    feat: view home

---
+++ src/app/components/views/view-home/view-home.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-view-home',
+  imports: [],
+  templateUrl: './view-home.html',
+  styleUrl: './view-home.css'
+})
+export class ViewHome {
+
+}
