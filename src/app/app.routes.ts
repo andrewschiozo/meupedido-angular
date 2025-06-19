992e2a52d47bafd18437cbb89f4d6fa594ebd265
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-06-19 16:31:37 -0300

    feat: implementação do router-outlet; definição do menu inicial

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { ViewHome } from './components/views/view-home/view-home';
+import { ViewPedido } from './components/views/view-pedido/view-pedido';
+import { ViewCliente } from './components/views/view-cliente/view-cliente';
+import { ViewProduto } from './components/views/view-produto/view-produto';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+    { path: 'home', component: ViewHome },
+    { path: 'pedido', component: ViewPedido },
+    { path: 'cliente', component: ViewCliente },
+    { path: 'produto', component: ViewProduto },
+];
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterModule, RouterOutlet } from '@angular/router';
+import { Menu } from "./components/menu/menu";
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.css'
+    selector: 'app-root',
+    imports: [Menu, RouterModule, RouterOutlet],
+    templateUrl: './app.html',
+    styleUrl: './app.css'
 })
 export class App {
-  protected title = 'meu-pedido';
+    protected title = 'Meu Pedido';
 }
