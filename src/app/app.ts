627ecdb4c4f1ac639e50edb26046bd33f037b7e9
Andrews Costa Chiozo <andrewschiozo@gmail.com>
2025-06-19 23:17:11 -0300

    feat: serviço de store por injeção

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
 import { Menu } from "./components/menu/menu";
+import { Store } from "./services/store/store.interface";
+import { StoreCliente } from './services/store/store-cliente';
 
 @Component({
     selector: 'app-root',
@@ -10,4 +12,14 @@ import { Menu } from "./components/menu/menu";
 })
 export class App {
     protected title = 'Meu Pedido';
+    services = {
+        store: {
+            cliente: {} as Store
+        }
+    };
+
+    public constructor()
+    {
+        this.services.store.cliente = StoreCliente.getInstance();
+    }
 }
